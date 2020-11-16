1cb18d26e4d656973e13d7927e0568cc7a4402cd
Davide Caputo <dave.caputo93@gmail.com>
2020-11-16 18:37:25 +0100

    add insecure random apis

---
+++ src/motan/ios_vulnerabilities/insecure_random/insecure_random.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import logging
+from typing import Optional, List
+import motan.categories as categories
+from motan import vulnerability as vuln
+from motan.analysis import IOSAnalysis
+import subprocess
+import os
+from pathlib import Path
+import lief
+import re
+
+
+class InsecureRandom(categories.ICodeVulnerability):
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        super().__init__()
+
+    def check_vulnerability(
+        self, analysis_info: IOSAnalysis
+    ) -> Optional[vuln.VulnerabilityDetails]:
+        self.logger.debug(f"Checking '{self.__class__.__name__}' vulnerability")
+        
+        try:
+            bin_path = Path(analysis_info.bin_path)
+            macho_object = lief.parse(bin_path.as_posix())
+            details = vuln.get_vulnerability_details(
+                os.path.dirname(os.path.realpath(__file__)), analysis_info.language
+            )
+            details.id = self.__class__.__name__
+
+            vulnerability_found = False
+            symbols = "\n".join([x.name for x in macho_object.symbols])
+
+            # TODO add configuration file where the plugin read the name of API
+            random = re.findall(
+                '_srando'|'_random', symbols)
+            random_api = list(set(random))
+
+            if len(random_api) > 0:
+                vulnerability_found = True
+                details.api = ", ".join(random_api)
+
+            if vulnerability_found:
+                return details
+            else:
+                return None
+
+        except Exception as e:
+            self.logger.error(
+                f"Error during '{self.__class__.__name__}' vulnerability check: {e}"
+            )
+            raise
+        
+        finally:
+            analysis_info.checked_vulnerabilities.append(self.__class__.__name__)
