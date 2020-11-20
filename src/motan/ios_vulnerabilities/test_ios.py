527f3eee0c5e27905f7e2d3b00e5f62aef0a248f
Auto Black Formatter <claudiugeorgiu@users.noreply.github.com>
2020-11-20 16:35:41 +0000

    Format code with Black [auto]

--- src/motan/ios_vulnerabilities/test_ios.py
+++ src/motan/ios_vulnerabilities/test_ios.py
@@ -7,7 +7,9 @@ from biplist import readPlist, writePlistToString
 ipa_file = sys.argv[1]
 working_dir = "working_dir_ios"
 
-zipfile_output = os.path.join(working_dir, f"{os.path.splitext(os.path.basename(ipa_file))[0]}.zip")
+zipfile_output = os.path.join(
+    working_dir, f"{os.path.splitext(os.path.basename(ipa_file))[0]}.zip"
+)
 shutil.copy2(ipa_file, zipfile_output)
 name_subdir = ""
 name_binary = ""
@@ -19,11 +21,11 @@ with zipfile.ZipFile(zipfile_output, "r") as zipfile_output_ipa:
         file_split = normpath.split(os.sep)
 
         if (
-                file_split[0] == "Payload"
-                and len(file_split) > 2
-                and file_split[1].endswith(".app")
-                and file_split[2].endswith(".plist")
-                and file_split[2].lower() == "info.plist"
+            file_split[0] == "Payload"
+            and len(file_split) > 2
+            and file_split[1].endswith(".app")
+            and file_split[2].endswith(".plist")
+            and file_split[2].lower() == "info.plist"
         ):
             name_subdir = file_split[1].split(".app")[0]
             read_content_plist = zipfile_output_ipa.read(entry)
@@ -34,7 +36,7 @@ with zipfile.ZipFile(zipfile_output, "r") as zipfile_output_ipa:
                 plist.write(read_content_plist)
 
             plist_readable = readPlist(os.path.join(ouput_dir, "Info.plist"))
-            bin_name = plist_readable.get('CFBundleExecutable', '')
+            bin_name = plist_readable.get("CFBundleExecutable", "")
 
 
 with zipfile.ZipFile(zipfile_output, "r") as zipfile_output_ipa:
@@ -43,10 +45,10 @@ with zipfile.ZipFile(zipfile_output, "r") as zipfile_output_ipa:
         file_split = normpath.split(os.sep)
 
         if (
-                file_split[0] == "Payload"
-                and len(file_split) > 2
-                and file_split[1].endswith(".app")
-                and file_split[2] == bin_name
+            file_split[0] == "Payload"
+            and len(file_split) > 2
+            and file_split[1].endswith(".app")
+            and file_split[2] == bin_name
         ):
             name_subdir = file_split[1].split(".app")[0]
             ouput_dir = os.path.join(working_dir, name_subdir)
--- src/motan/util.py
+++ src/motan/util.py
@@ -113,7 +113,9 @@ def get_list_cpu_type(name_binary):
 
 def unpacking_ios_app(ipa_path: str, working_dir: str):
 
-    zipfile_output = os.path.join(working_dir, f"{os.path.splitext(os.path.basename(ipa_path))[0]}.zip")
+    zipfile_output = os.path.join(
+        working_dir, f"{os.path.splitext(os.path.basename(ipa_path))[0]}.zip"
+    )
     shutil.copy2(ipa_path, zipfile_output)
     name_subdir = ""
     name_binary = ""
@@ -125,11 +127,11 @@ def unpacking_ios_app(ipa_path: str, working_dir: str):
             file_split = normpath.split(os.sep)
 
             if (
-                    file_split[0] == "Payload"
-                    and len(file_split) > 2
-                    and file_split[1].endswith(".app")
-                    and file_split[2].endswith(".plist")
-                    and file_split[2].lower() == "info.plist"
+                file_split[0] == "Payload"
+                and len(file_split) > 2
+                and file_split[1].endswith(".app")
+                and file_split[2].endswith(".plist")
+                and file_split[2].lower() == "info.plist"
             ):
                 name_subdir = file_split[1].split(".app")[0]
                 read_content_plist = zipfile_output_ipa.read(entry)
@@ -140,7 +142,7 @@ def unpacking_ios_app(ipa_path: str, working_dir: str):
                     plist.write(read_content_plist)
 
                 plist_readable = readPlist(os.path.join(ouput_dir, "Info.plist"))
-                bin_name = plist_readable.get('CFBundleExecutable', '')
+                bin_name = plist_readable.get("CFBundleExecutable", "")
 
     with zipfile.ZipFile(zipfile_output, "r") as zipfile_output_ipa:
         for entry in zipfile_output_ipa.infolist():
@@ -148,10 +150,10 @@ def unpacking_ios_app(ipa_path: str, working_dir: str):
             file_split = normpath.split(os.sep)
 
             if (
-                    file_split[0] == "Payload"
-                    and len(file_split) > 2
-                    and file_split[1].endswith(".app")
-                    and file_split[2] == bin_name
+                file_split[0] == "Payload"
+                and len(file_split) > 2
+                and file_split[1].endswith(".app")
+                and file_split[2] == bin_name
             ):
                 name_subdir = file_split[1].split(".app")[0]
                 ouput_dir = os.path.join(working_dir, name_subdir)
